83e7fdf6bbfd90e8135bc9f8a074815224e656a2
Cwen Fernandes <91659975+Rayonushi@users.noreply.github.com>
2024-03-02 18:33:11 +0530

    Update JD_Sample.py

--- JD_Sample.py
+++ JD_Sample.py
@@ -3,13 +3,13 @@ from Preprocessing_Parsing import ResumeProcessor
 class Match:
     def jd_skill(self,jd):
         resume_processor = ResumeProcessor()
-        resume_processor.load_skill_patterns("D:\\NLP_CaseStudy\\jz_skill_patterns.jsonl")
+        resume_processor.load_skill_patterns("jz_skill_patterns.jsonl")
         jd_skills = resume_processor.extracting_entities(jd)["SKILL"]
         return jd_skills
 
     def find_not_in_resume(self,resume, jd):
         resume_processor = ResumeProcessor()
-        resume_processor.load_skill_patterns("D:\\NLP_CaseStudy\\jz_skill_patterns.jsonl")
+        resume_processor.load_skill_patterns("jz_skill_patterns.jsonl")
         # Extract Resume Skills
         resume_skills = resume_processor.extracting_entities(resume)["SKILL"]
         # Extracting Job Description Skills
